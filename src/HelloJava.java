9f9139c9aee5ecb7c54bb5a68b3d21ea54580a4b
ChanduYanduri <chandu.yanduri@gmail.com>
2018-02-04 13:04:31 +0000

    My first commit added on 04.02.18.txt

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+public static void main(String[] args){
+    String greeting = "Hi Hello How are you";
+    System.out.println(greeting);
+    System.out.print("Hello");
+    System.out.println(" World");
+    System.out.println("Hello World");
+}
+}
